a782f0ebb2232c9f8fd0f83a6ce0e18bb7192c95
Joshua Longerbeam <josh.terainsights@outlook.com>
2016-01-13 14:02:09 -0500

    Ready for production use

--- src/angular-secure-password.js
+++ src/angular-secure-password.js
@@ -4,10 +4,10 @@ angular.module('angular-secure-password', []).directive('securePassword',
       restrict: 'E',
       replace: false,
       transclude: false,
-      scope: { buffer: '=typedarray'/*, utf: '=?'*/ },
+      scope: { buffer: '=typedarray', extended: '=?' },
 
       template: '<div style="margin-bottom: 10px;">'+
-                  '<span class="secureAM" ng-keypress="append($event)" ng-keydown="limitedAlter($event)" tabindex="-1">'+
+                  '<span class="secure-input" ng-keypress="append($event)" ng-keydown="alter($event)" tabindex="0">'+
                     '<span ng-repeat="char in notPassword">'+
                       '<span ng-if="$first" style="word-spacing: -10px;">&nbsp;</span>'+
                       '<span class="password-symbol">&bull;&nbsp;</span>'+
@@ -17,48 +17,54 @@ angular.module('angular-secure-password', []).directive('securePassword',
                   '</span>'+
                 '</div>',
                 
-      link: function(scope, element, attrs) {
-        // console.log(scope.buffer.constructor.name);
-        
-        var cursor = 0;
-        var size = scope.buffer.length;
-        scope.notPassword = [];
-        // if(typeof scope.utf === 'undefined')
-        //   scope.utf = scope.buffer.byteLength;
-        
-        scope.clearAll = function() {
-          cursor = 0;
-          scope.buffer.fill(0);
+      link: {
+        pre: function prelink(scope, element, attrs){
+          var cursor = 0;
+          var size = scope.buffer.length;
           scope.notPassword = [];
-        };
-        
-        scope.append = function(keyEvent) {
-          // console.log(keyEvent);
-          /// only ascii printable characters allowed (currently)
-          if(keyEvent.charCode >= 32 && keyEvent.charCode <= 126) {
-            if (cursor >= 0 && cursor < size-1) {
-              scope.buffer[cursor] = keyEvent.charCode;
-              scope.notPassword.push(cursor);
-              cursor++;
-            }
-          }
-          stopEvent(keyEvent);
-        };
-        
-        scope.limitedAlter = function(keyEvent){
-          // console.log(keyEvent);
-          if(keyEvent.keyCode !== 8) //&& keyEvent.keyIdentifier === 'U+0008')
-            return;
-          else {
-            if (cursor > 0) {
+          
+          // // not sure how to expose directive function to parent controller
+          // scope.clear = function() {
+          //   cursor = 0;
+          //   scope.buffer.fill(0);
+          //   scope.notPassword = [];
+          // };
+          
+          scope.alter = function(keyEvent){
+            // console.log(keyEvent);
+            if (keyEvent.keyCode === 8 && cursor > 0) { // || keyEvent.keyIdentifier === 'U+0008')
               scope.buffer[--cursor] = 0;
               scope.notPassword.splice(-1, 1);
+              stopEvent(keyEvent); // to prevent backspace from navigating backwards in browser history
             }
-            stopEvent(keyEvent);
-          }
-        };
-        
-        var stopEvent = function(e) { if (e) { e.preventDefault(); if (e.stopPropagation) {e.stopPropagation(); } else {e.cancelBubble = true; } } };
+          };
+          
+          //// NOTE: Functionality of extended ASCII has not been tested whatsoever
+          scope.append = function(keyEvent) {
+            // console.log(keyEvent);
+            var cC = keyEvent.charCode;
+            // console.log(cC);
+            var cond1 = cC >= 32 && cC <= 126,  //excludes 'DEL' char at 127
+                cond2 = scope.extended === true && cC >= 32 && cC <= 255 && (cC !== 129 && cC !== 141 && cC !== 143 && cC !== 144 && cC !== 157);
+            if (cursor >= 0 && cursor < size) {
+              if(cond1 || cond2) {
+                scope.buffer[cursor] = cC;
+                scope.notPassword.push(cursor);
+                cursor++;
+                stopEvent(keyEvent);
+              }
+            }
+          };
+          
+          scope.$watch('buffer[0]', function(newVal){
+            if(newVal === 0) {  // .fill(0) called from parent
+              scope.notPassword = []; // reset corresponding vars
+              cursor = 0;
+            }
+          });
+          
+          var stopEvent = function(e) { if (e) { e.preventDefault(); if (e.stopPropagation) { e.stopPropagation(); } else { e.cancelBubble = true; } } };
+        }
       }
     };
   });
