590fca0670de305bf7a9e30a23341a45ad8e8120
Joshua Longerbeam <josh.terainsights@outlook.com>
2016-01-13 11:21:30 -0500

    That thing's operational! -Lando

--- src/angular-secure-password.js
+++ src/angular-secure-password.js
@@ -4,11 +4,7 @@ angular.module('angular-secure-password', []).directive('securePassword',
       restrict: 'E',
       replace: false,
       transclude: false,
-      scope: {
-        // buff: '=arraybuffer',  // I have no idea what I'm doing
-        // loggedin: '=',
-        // oeprvkey: '=' // both set after successful login
-      },
+      scope: { buffer: '=typedarray'/*, utf: '=?'*/ },
 
       template: '<div style="margin-bottom: 10px;">'+
                   '<span class="secureAM" ng-keypress="append($event)" ng-keydown="limitedAlter($event)" tabindex="-1">'+
@@ -20,55 +16,49 @@ angular.module('angular-secure-password', []).directive('securePassword',
                     '<span class="placeholder" ng-if="notPassword.length===0">Password</span>'+
                   '</span>'+
                 '</div>',
+                
       link: function(scope, element, attrs) {
-        // scope.data = {
-        //     key: false, // flag if file with key is uploaded
-        //     keypair: {} // keypair
-        // };
-      },
-
-      controller: ['$scope',
-        function($scope) {
-          var cursor = 0;
-          $scope.buff = new Uint8Array(100);
-          $scope.decoder = new TextDecoder('utf-8');
-          $scope.notPassword = [];
-
-          $scope.clearAll = function() {
-            cursor = 0;
-            $scope.buff.fill(0);
-            $scope.notPassword = [];
-          };
-
-          $scope.append = function(keyEvent) {
-            // console.log(keyEvent);
-            /// only ascii printable characters allowed (currently)
-            if(keyEvent.charCode >= 32 && keyEvent.charCode <= 126) {
-              if (cursor >= 0 && cursor < 99) {
-                $scope.buff[cursor] = keyEvent.charCode;
-                $scope.notPassword.push(cursor);
-                cursor++;
-              }
+        // console.log(scope.buffer.constructor.name);
+        
+        var cursor = 0;
+        var size = scope.buffer.length;
+        scope.notPassword = [];
+        // if(typeof scope.utf === 'undefined')
+        //   scope.utf = scope.buffer.byteLength;
+        
+        scope.clearAll = function() {
+          cursor = 0;
+          scope.buffer.fill(0);
+          scope.notPassword = [];
+        };
+        
+        scope.append = function(keyEvent) {
+          // console.log(keyEvent);
+          /// only ascii printable characters allowed (currently)
+          if(keyEvent.charCode >= 32 && keyEvent.charCode <= 126) {
+            if (cursor >= 0 && cursor < size-1) {
+              scope.buffer[cursor] = keyEvent.charCode;
+              scope.notPassword.push(cursor);
+              cursor++;
+            }
+          }
+          stopEvent(keyEvent);
+        };
+        
+        scope.limitedAlter = function(keyEvent){
+          // console.log(keyEvent);
+          if(keyEvent.keyCode !== 8) //&& keyEvent.keyIdentifier === 'U+0008')
+            return;
+          else {
+            if (cursor > 0) {
+              scope.buffer[--cursor] = 0;
+              scope.notPassword.splice(-1, 1);
             }
             stopEvent(keyEvent);
-          };
-          
-          
-          $scope.limitedAlter = function(keyEvent){
-            // console.log(keyEvent);
-            if(keyEvent.keyCode !== 8) //&& keyEvent.keyIdentifier === 'U+0008')
-              return;
-            else {
-              if (cursor > 0) {
-                $scope.buff[--cursor] = 0;
-                $scope.notPassword.splice(-1, 1);
-              }
-              stopEvent(keyEvent);
-            }
-          };
-          
-          var stopEvent = function(e) { if (e) { e.preventDefault(); if (e.stopPropagation) {e.stopPropagation(); } else {e.cancelBubble = true; } } };
-        }
-      ]
+          }
+        };
+        
+        var stopEvent = function(e) { if (e) { e.preventDefault(); if (e.stopPropagation) {e.stopPropagation(); } else {e.cancelBubble = true; } } };
+      }
     };
   });
